e3dd49fcf052a02d0d0f1639e7f93fc6b1cd288f
Jerry Britto <jbritto2004@gmail.com>
2023-10-28 21:44:05 +0530

    Basic MERN website

--- Project 3/client/src/Layout.jsx
+++ Project 3/client/src/Layout.jsx
@@ -3,7 +3,10 @@ import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
 function Layout() {
   return (
-    <div>
+    <div
+      className='w-full h-screen bg-cover'
+      style={{backgroundImage:`url('https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1698451200&semt=sph')`}}
+    >
       <Header/>
       <Outlet/>
     </div>
